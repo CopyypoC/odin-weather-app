16eabba5b90fa2663b788619d5452d54d2e983cd
CopyypoC <william.wang650@gmail.com>
2025-04-15 03:30:44 -0700

    Add getGifUrl

---
+++ src/scripts/get-gif.js
@@ -0,0 +1,14 @@
+const GIPHY_API_KEY = "no7wHXrEXsgBmDumUyeuT4u0T8RNiqse";
+
+export async function getGifUrl(condition) {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_API_KEY}&s=${condition}`,
+    );
+    const json = await response.json();
+    console.log(json.data.images.original.url);
+    return json.data.images.original.url;
+  } catch (err) {
+    console.log(err);
+  }
+}
