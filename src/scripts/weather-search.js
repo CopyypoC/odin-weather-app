07407eb60869f3eb5ba0891b26ca78b20ad15e51
CopyypoC <william.wang650@gmail.com>
2025-04-15 03:44:16 -0700

    Add gif display on search

--- src/scripts/weather-search.js
+++ src/scripts/weather-search.js
@@ -1,14 +1,30 @@
 import { getWeatherData } from "./get-weather-data";
+import { getGifUrl } from "./get-gif";
 
 const locationForm = document.querySelector(".location-form");
-const location = document.getElementById("location");
+const location = document.getElementById("location-input");
 
 locationForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const weatherData = await getWeatherData(location.value);
-  console.log(weatherData);
+  const gifUrl = await getGifUrl(weatherData.currentConditions.conditions);
+
+  displayGif(gifUrl);
+  location.value = "";
 });
 
+function displayWeatherData() {}
+
+const gifContainer = document.querySelector(".gif-container");
+function displayGif(url) {
+  const gif = document.createElement("img");
+
+  gif.src = url;
+  (gif.alt = "Weather GIF"), gif.classList.add("gif");
+
+  gifContainer.replaceChildren();
+  gifContainer.appendChild(gif);
+}
 // description: 'Cooling down with a chance of rain',
 // resolvedAddress: "London, England, United Kingdom"
 // currentConditions {
